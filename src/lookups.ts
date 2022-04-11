fec5dac44e846509005b49c81628d89d72f07fca
Léo Pradel <pradel.leo@gmail.com>
2022-04-11 19:13:18 +0200

    feat: setup typescript compilation and migrate lookup file

---
+++ src/lookups.ts
@@ -0,0 +1,92 @@
+import {
+  config as bskConfig,
+  validateProofs,
+  resolveZoneFileToProfile,
+} from "blockstack";
+import { validateStacksAddress } from "@stacks/transactions";
+import fetch from "node-fetch";
+
+import logger from "winston";
+
+export async function isSubdomainRegistered(fullyQualifiedAddress: String) {
+  try {
+    const nameInfoUrl =
+      // coreApiUrl does not exist in the blockstack types, but to not change the code we silence this type issue.
+      // @ts-ignore
+      bskConfig.network.coreApiUrl + "/v1/names/" + fullyQualifiedAddress;
+    const nameInfoRequest = await fetch(nameInfoUrl);
+    const { status } = nameInfoRequest;
+    const nameInfo = await nameInfoRequest.json();
+    if (status == 200) {
+      return nameInfo.status === "registered_subdomain";
+    } else {
+      return false;
+    }
+  } catch (err) {
+    if (err.message === "Name not found") {
+      return false;
+    } else if (err.message === "Bad response status: 500") {
+      return false; // currently, the blockstack api returns 500 on subdomain lookup errors.
+    } else {
+      throw err;
+    }
+  }
+}
+
+export function validlySignedUpdate() {
+  throw new Error("Not implemented");
+}
+
+export function checkProofs(owner: string, zonefile: any) {
+  return (
+    resolveZoneFileToProfile(zonefile, owner)
+      // validateProofs expects a 3rd argument (cheerio instance), but to not change the code we don't use it.
+      // @ts-ignore
+      .then((profile) => validateProofs(profile, owner))
+      .then((proofs) => proofs.filter((x) => x.valid))
+  );
+}
+
+export async function isRegistrationValid(
+  subdomainName: string,
+  domainName: string,
+  owner: string,
+  sequenceNumber: number,
+  checkCore: boolean
+) {
+  // currently, only support *new* subdomains
+  if (sequenceNumber !== 0) {
+    logger.debug(`seqn: ${sequenceNumber} failed validation`);
+    return false;
+  }
+
+  // owner should be a stacks address
+  if (!validateStacksAddress(owner)) {
+    logger.debug(`owner: ${owner} failed validation`);
+    return false;
+  }
+
+  // subdomain name should be a legal name
+  const subdomainRegex = /^[a-z0-9\-_+]{1,37}$/;
+  if (!subdomainRegex.test(subdomainName)) {
+    logger.debug(`subdomainName: ${subdomainName} failed validation`);
+    return false;
+  }
+  if (!checkCore) {
+    return true;
+  }
+
+  // shouldn't already exist
+  try {
+    const isRegistered = await isSubdomainRegistered(
+      `${subdomainName}.${domainName}`
+    );
+    if (isRegistered) {
+      logger.warn(`${subdomainName}.${domainName} already exists`);
+    }
+    return !isRegistered;
+  } catch (e) {
+    logger.error(e);
+    return false;
+  }
+}
